653232f4a6a519caab12fc9901a4b04bf4cf90bb
Victory <vickywane@gmail.com>
2021-12-07 10:27:21 +0100

    update: removed unused assets

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,72 @@
 <template>
   <div>
-    <h1> HELLO THERE IN APP </h1>
+    <Header />
+
+    <div class="align-center">
+      <div>
+        <div class="img-ctn">
+          <img
+            src="../assets/logo-vuejs.png"
+            alt="Vue.js"
+          >
+          <img
+            src="../assets/cypress.png"
+            alt="Vue.js"
+          >
+          <img
+            src="../assets/jest.png"
+            alt="Vue.js"
+          >
+        </div>
+        <h1> A minimal Vue.js boilerplate tested <br /> with Cypress and Jest. </h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from './components/header.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  }
 }
 </script>
+
+<style scoped>
+.align-center {
+  text-align: center;
+  height: calc(100vh - 100px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  height: 150px;
+  object-fit: contain;
+}
+
+.img-ctn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+@media (max-width: 840px) {
+  .img-ctn {
+    flex-direction: column;
+  }
+
+  h1 {
+    font-size: 1.1rem;
+    /* text-align: right; */
+  }
+
+  img {
+    height: 100px;
+  }
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <nav>
+        <h2> Minimal Vue Boilerplate</h2>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+nav {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+@media (max-width: 840px) {
+  nav {
+      display: none;
+  }
+}
+</style>
